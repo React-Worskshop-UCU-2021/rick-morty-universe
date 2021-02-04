2cc06898b8511b187ec59ab407b7c223b2b2acf0
Oleh Mykytyn <oleh.m@volpis.com>
2021-02-05 01:49:25 +0200

    delay on search

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,24 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+import _ from 'lodash';
+
 import Hero from '../../components/Hero';
 import UserList from '../../components/UserList';
-
 import { getCharacters } from '../../api';
 
 import './Home.scss';
 
 function Home() {
     const [name, setName] = useState('');
+    const [queryName, setQueryName] = useState('');
     const [gender, setGender] = useState('');
     const [status, setStatus] = useState('');
     const [characters, setCharacters] = useState();
 
+    const delayedQuery = useCallback(_.debounce(setQueryName, 500), [
+        setQueryName,
+    ]);
+
     useEffect(() => {
         loadCharacters({
-            ...(name && { name }),
+            ...(queryName && { name: queryName }),
             ...(gender && { gender }),
             ...(status && { status }),
         });
-    }, [name, gender, status]);
+    }, [queryName, gender, status]);
 
     const loadCharacters = async (params) => {
         const items = await getCharacters(params);
@@ -29,11 +35,16 @@ function Home() {
         }
     };
 
+    const onNameChange = (value) => {
+        setName(value);
+        delayedQuery(value);
+    };
+
     return (
         <div className="Home">
             <Hero
                 characterName={name}
-                setCharacterName={setName}
+                setCharacterName={onNameChange}
                 gender={gender}
                 setGender={setGender}
                 status={status}
